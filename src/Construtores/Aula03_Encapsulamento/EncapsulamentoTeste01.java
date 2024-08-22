cd19517f7a7719b08b27b8127b63df9502806849
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-08-22 20:01:12 -0300

    Construtores

---
+++ src/Construtores/Aula03_Encapsulamento/EncapsulamentoTeste01.java
@@ -0,0 +1,28 @@
+package Construtores.Aula03_Encapsulamento;
+
+
+public class EncapsulamentoTeste01 {
+    public static void main(String[] args) {
+
+        Usuario usuario = new Usuario("Lucas", "lucas@gmail.com", "123456", "888.122.421-32");
+
+        System.out.println("Nome usuário: " + usuario.getNome());
+        System.out.println("Email usuário: " + usuario.getEmail());
+        System.out.println("Senha usuário: " + usuario.getPassword());
+        System.out.println("CPF usuário: " + usuario.getCpf());
+
+        System.out.println();
+
+        // Note que não temos o metodo setter para o cpf, ou seja, o usuário nao pode alterar o cpf. Dessa
+        // forma criamos uma barreira e mantemos os dados da nossa classe seguros.
+
+        usuario.setNome("Silva");
+        usuario.setEmail("Silva@gmail.com");
+        usuario.setPassword("1384");
+
+        System.out.println("Nome usuário: " + usuario.getNome());
+        System.out.println("Email usuário: " + usuario.getEmail());
+        System.out.println("Senha usuário: " + usuario.getPassword());
+        System.out.println("CPF usuário: " + usuario.getCpf());
+    }
+}
---
+++ src/Construtores/Aula03_Encapsulamento/Usuario.java
@@ -0,0 +1,54 @@
+package Construtores.Aula03_Encapsulamento;
+
+public class Usuario {
+
+    private String cpf;
+    private String nome;
+    private String email;
+    private String password;
+
+    public Usuario(String nome, String email, String password, String cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
