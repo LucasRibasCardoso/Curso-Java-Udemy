16b594ea715d6d3f79d2d76dbbf35fa8cd4f2f28
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-09-12 18:02:17 -0300

    exercicio explicativo interface

---
+++ src/Modulo08/ExercicioInterface/models/entities/Vehicle.java
@@ -0,0 +1,12 @@
+package Modulo08.ExercicioInterface.models.entities;
+
+public class Vehicle {
+    private String model;
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+    public String getModel() {
+        return model;
+    }
+}
---
+++ src/Modulo08/ExercicioInterface/models/services/TaxService.java
@@ -0,0 +1,5 @@
+package Modulo08.ExercicioInterface.models.services;
+
+public interface TaxService {
+    double tax(double amount);
+}
