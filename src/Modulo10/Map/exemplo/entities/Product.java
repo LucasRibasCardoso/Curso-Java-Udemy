6f2ad99ca118616b5fedb8e54d5a6b79a9a1ebea
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-09-24 16:31:57 -0300

    exemplos de implementação Map

---
+++ src/Modulo10/Map/exemplo/entities/Product.java
@@ -0,0 +1,34 @@
+package Modulo10.Map.exemplo.entities;
+
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(getName(), product.getName()) && Objects.equals(getPrice(), product.getPrice());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getPrice());
+    }
+}
